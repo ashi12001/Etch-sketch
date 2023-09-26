8e5810b931223960ec7142206db0b06778181ef5
ASHI <ashi@ASHIs-MacBook-Air.local>
2023-09-26 15:26:14 +0530

    prettier and clean  version with Eraser

--- work.js
+++ work.js
@@ -1,41 +1,99 @@
 const container = document.querySelector(".container");
+// let widthOfContainer=parseInt(prompt("How many grids you want in a row or column"));
+let widthOfContainer = 16;
 
-for (let i = 1; i <= 16 * 16; i++) {
-  let div = document.createElement("div");
-
-  container.appendChild(div);
-  let n = 600 / 16;
-  div.style.cssText = `width:${n}px;height:${n}px;`;
+if (widthOfContainer > 61) {
+  alert("we don't support too much grids Try lower");
+  widthOfContainer = 60;
 }
-// const div1 = document.createElement('div');
-// div1.classList.add('div1');
-// container.appendChild(div1);
 
-let divv = document.querySelectorAll(".container div");
+//THis method i use is for width of container but it is not optimal
 
-//hover Effect
-divv.forEach((button) => {
-  // and for each one we add a 'click' listener
-  button.addEventListener("mouseenter", () => {
-    button.style.backgroundColor = "black";
-  });
-});
+let newDimensions = 600;
+container.style.width = newDimensions + "px";
+container.style.height = newDimensions + "px";
 
-//creating reset button
+function createContainer() {
+  for (let i = 1; i <= widthOfContainer * widthOfContainer; i++) {
+    let divv = document.createElement("div");
+    container.appendChild(divv);
+    let n = newDimensions / widthOfContainer - 1;
 
-function reset() {
-  const resetBtn = document.createElement("button");
-  resetBtn.textContent = "RESET";
-  container.appendChild(resetBtn);
-  resetBtn.setAttribute('id',"reset");
+    divv.style.cssText = `width:${n}px;height:${n}px;`;
+  }
+}
+createContainer();
 
-  resetBtn.addEventListener("click", () => {
-    divv.forEach((button) => {
-      button.style.backgroundColor = "white";
+//hover
+const divv = document.querySelectorAll(".container div");
+
+function getHoverEffect() {
+  //hover Effect
+  divv.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener("mouseenter", () => {
+      button.style.backgroundColor = "black";
     });
   });
 }
+getHoverEffect();
 
+//eraser
+const controls = document.querySelector(".controls");
+
+function reset() {
+  const reset = document.createElement("button");
+  reset.setAttribute("id", "reset");
+  reset.textContent = "Reset";
+  controls.appendChild(reset);
+
+  reset.addEventListener("click", () => {
+    divv.forEach((individual) => {
+      individual.style.backgroundColor = "white";
+    });
+  });
+}
 reset();
 
+//eraser
 
+function eraser() {
+  const eraser = document.createElement("button");
+  eraser.setAttribute("id", "eraser");
+  eraser.textContent = "Eraser";
+  controls.appendChild(eraser);
+
+  eraser.addEventListener("click", () => {
+    divv.forEach((button) => {
+      // and for each one we add a 'click' listener
+      button.addEventListener("mouseenter", () => {
+        button.style.backgroundColor = "white";
+      });
+    });
+  });
+}
+eraser();
+
+function randColor() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return `#${randomColor}`;
+}
+
+// rainbow hover effext
+
+function rainbowHover() {
+  const rainbowButton = document.createElement("button");
+  rainbowButton.textContent = "rainbow";
+  controls.appendChild(rainbowButton);
+  rainbowButton.setAttribute("id", "rainbow");
+
+  rainbowButton.addEventListener("click", () => {
+    divv.forEach((button) => {
+      // and for each one we add a 'click' listener
+      button.addEventListener("mouseenter", () => {
+        button.style.backgroundColor = randColor();
+      });
+    });
+  });
+}
+rainbowHover();
